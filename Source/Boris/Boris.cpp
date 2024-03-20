11121fc90de3d761b45885ceb09515d9cf1271e0
luisgvecchio <luisgvecchio@gmail.com>
2024-03-20 17:04:30 +0100

    Initial commit

---
+++ Source/Boris/Boris.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Boris.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Boris, "Boris" );
