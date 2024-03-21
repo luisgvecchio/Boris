abf115dc7f9eb24d42b8316c0bd64277cdd561ec
luisgvecchio <luisgvecchio@gmail.com>
2024-03-21 21:45:13 +0100

    aded healing with Gameplay Effects

--- Source/Boris/Private/Actor/BorisEffectActor.cpp
+++ Source/Boris/Private/Actor/BorisEffectActor.cpp
@@ -3,10 +3,8 @@
 
 #include "Actor/BorisEffectActor.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/BorisAttributeSet.h"
-#include "Components/SphereComponent.h"
 
 // Sets default values
 ABorisEffectActor::ABorisEffectActor()
@@ -15,36 +13,24 @@ ABorisEffectActor::ABorisEffectActor()
 
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
-void ABorisEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack! (Remove const_cast)
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UBorisAttributeSet* BorisAttributeSet = Cast<UBorisAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UBorisAttributeSet::StaticClass()));
-
-		UBorisAttributeSet* MutableBorisAttributeSet = const_cast<UBorisAttributeSet*>(BorisAttributeSet);
-		MutableBorisAttributeSet->SetHealth(BorisAttributeSet->GetHealth() + 25.f);
-		Destroy();
-	}
-}
-
-void ABorisEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-}
 
 // Called when the game starts or when spawned
 void ABorisEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ABorisEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &ABorisEffectActor::EndOverlap);
 }
 
+void ABorisEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if (TargetASC == nullptr) return;
+
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+}
--- Source/Boris/Public/Actor/BorisEffectActor.h
+++ Source/Boris/Public/Actor/BorisEffectActor.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "BorisEffectActor.generated.h"
 
-class USphereComponent;
+class UGameplayEffect;
 
 UCLASS()
 class BORIS_API ABorisEffectActor : public AActor
@@ -14,20 +14,16 @@ class BORIS_API ABorisEffectActor : public AActor
 	GENERATED_BODY()
 	
 public:	
+
 	ABorisEffectActor();
-
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
 protected:
+
 	virtual void BeginPlay() override;
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 };
